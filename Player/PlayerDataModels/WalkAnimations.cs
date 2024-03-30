59b93f654620beb399878a7946358647c779f15c
Albert J Berger <ajberger2013@gmail.com>
2024-03-30 17:58:59 -0500

    Partway through animations tutorial

---
+++ Player/PlayerDataModels/WalkAnimations.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActionRPGTutorial.Player.PlayerDataModels
+{
+	internal static class WalkAnimations
+	{
+		public static string WalkDown { get; } = "WalkDown";
+		public static string WalkLeft { get; } = "WalkLeft";
+		public static string WalkRight { get; } = "WalkRight";
+		public static string WalkUp { get; } = "WalkUp";
+	};
+}
--- player/player.cs
+++ player/player.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Runtime.CompilerServices;
+using ActionRPGTutorial.Player.PlayerDataModels;
 using Godot;
 
 namespace ActionRPGTutorial.Player;
@@ -6,15 +9,58 @@ public partial class Player : CharacterBody2D
 {
 	[Export]
 	public int Speed { get; set; } = 35;
-	public void HandleInput()
-	{
-		var moveDirection = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		Velocity = moveDirection * Speed;
-	}
 
+	private AnimationPlayer _animationPlayer = new();
+
+	/*
+	 * Overrides
+	 */
+	public override void _Ready()
+	{
+		_animationPlayer = GetNode<AnimationPlayer>("PlayerAnimations");
+	}
 	public override void _PhysicsProcess(double delta)
 	{
 		HandleInput();
 		MoveAndSlide();
+	}
+
+	/*
+	 * Custom Methods
+	 */
+	public void HandleInput()
+	{
+		var moveDirection = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		Velocity = moveDirection * Speed;
+		UpdateAnimation();
+	}
+
+	public void UpdateAnimation()
+	{
+		var direction = WalkAnimations.WalkDown;
+
+		switch (Velocity.X)
+        {
+            case < 0:
+                direction = WalkAnimations.WalkLeft;
+                break;
+            case > 0:
+                direction = WalkAnimations.WalkRight;
+                break;
+            default:
+            {
+                if (Velocity.Y < 0)
+                {
+                    direction = WalkAnimations.WalkUp;
+                }
+                else if (Velocity is { X: 0, Y: 0 })
+                {
+                    direction = "RESET";
+                }
+
+                break;
+            }
+        }
+		_animationPlayer.Play(direction);
 	}
 }
